0dd83bc4f1f1e15034a0540e68f3e96555f19d47
Chris Luiz <chris@chrisluiz.io>
2017-06-27 21:07:31 -0400

    uncommitted changes

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, jsonify, render_template
+import sqlite3
 
 app = Flask(__name__)
+
+@app.route('/calc')
+def calc():
+    # connection
+    with sqlite3.connect('calc.db') as connection:
+        # create cursor
+        c = connection.cursor()
+        # execute sql statement
+        c.execute("""SELECT * FROM calculations""")
+        rows = c.fetchall()
+        # iterate through returned rows
+            # create some sort of container
+        # return data
+    return 'test'
 
 @app.route('/calc/<operator>/<int:num1>/<int:num2>')
 def add(operator, num1, num2):
@@ -19,18 +34,6 @@ def add(operator, num1, num2):
             'solution': total
             }
     return render_template('index.html', data=data)
-
-# @app.route('/calc/<operator>/<int:num1>/<int:num2>')
-# def add(operator, num1, num2):
-#     if operator == 'add':
-#         total = num1 + num2
-#         return_obj = {
-#             'operator': operator,
-#             'num1': num1,
-#             'num2': num2,
-#             'solution': total
-#         }
-#         return jsonify(return_obj)
 
 if __name__ =='__main__':
     app.run(debug=True)
